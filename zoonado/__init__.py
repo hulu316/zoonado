864b2939e305650e418e881545dc486b6f88200e
William Glass <william.glass@gmail.com>
2016-04-14 01:21:27 -0700

    Bump version to 0.8.1

--- zoonado/__init__.py
+++ zoonado/__init__.py
@@ -3,6 +3,6 @@ from .protocol import WatchEvent  # noqa
 from .protocol.acl import ACL  # noqa
 from .retry import RetryPolicy  # noqa
 
-version_info = (0, 8, 0)
+version_info = (0, 8, 1)
 
 __version__ = ".".join((str(point) for point in version_info))
